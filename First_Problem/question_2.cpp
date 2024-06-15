feba0bf977468a51ca2be268cf2363232e755086
Prakash Bhati <122536126+Prakashbhati086@users.noreply.github.com>
2024-06-15 15:05:11 +0530

    Delete First_Problem directory

--- First_Problem/question_2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    char s = 'U';
-    int asciiValue = static_cast<int>(s);
-    cout << "ASCII value " << s << " is: " << asciiValue << endl;
-    return 0;
-}
